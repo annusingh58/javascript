695c1c1ba86313422847dda8028181b8e536bd0a
annusingh58 <127593919+annusingh58@users.noreply.github.com>
2023-04-02 17:54:49 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+var a= 1234
+console.log (a)
+console.log ( typeof(a))
+var b="anu"
+console.log(b)
+console.log ( typeof(b))
+var c='asmaan'
+console.log(c)
+console.log ( typeof(c))
+var d='amaan';
+console.log(d)
+console.log ( typeof(d))
+
