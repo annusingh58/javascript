116b39e176ea06ade0a4e459a5916d1f2d1ad7c2
annusingh58 <127593919+annusingh58@users.noreply.github.com>
2023-04-05 09:56:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
-var a= 1234
-console.log (a)
-console.log ( typeof(a))
-var b="anu"
-console.log(b)
-console.log ( typeof(b))
-var c='asmaan'
-console.log(c)
-console.log ( typeof(c))
-var d='amaan';
-console.log(d)
-console.log ( typeof(d))
+// var a= 1234
+// console.log (a)
+// console.log ( typeof(a))
+// var b="anu"
+// console.log(b)
+// console.log ( typeof(b))
+// var c='asmaan'
+// console.log(c)
+// console.log ( typeof(c))
+// var d='amaan';
+// console.log(d)
+// console.log ( typeof(d))
 
+// var e=1234567;
+// var f=1234567
+// var add=e+f;
+// console.log(add)
+
+// var e=1234567;
+// var f="1234567"
+// console.log (e==f) //only check data
+// console.log(e===f) //  check data and type both
+
+var a=12345;
+var b=56789;
+console.log(a && b) //  is defined second var . in this example  print b
+
+console.log(a || b) // defined first var. if first is not given then show undefined.
