0d544d902b191cc4e75c85a3bcfce14f12974ae3
annusingh58 <127593919+annusingh58@users.noreply.github.com>
2023-04-07 08:34:43 -0400

    updates

---
+++ server1.js
@@ -0,0 +1,40 @@
+// console.log (2 == "2") //equal?
+// console.log (2 != "2") // no equal?
+// console.log (2 === "2")  //equal?
+// console.log (2 !=="2") // no equal?
+
+
+//condition statement
+//console.log(2>1)
+//if else
+//switch
+
+// if(2>1){
+//     console.log("student")
+//     console.log("Name")
+// }
+// else{
+//     console.log("asmaan")
+//     console.log("awdiz")
+// }
+
+//make a driving licence website,
+//allow users for dl if there age is above 18,
+//if user age is 18 then allow him for LR,
+
+var userage=90;
+if (userage>80){
+    console.log("you are over age")
+}
+else if(userage ==18){
+    console.log("you are allow for learing licence")
+
+}
+else if(userage>18){
+    console.log("you are allow for driving licence")
+ 
+}
+else{
+    console.log("you are under age for learing licence")
+
+}
---
+++ serverswitch.js
@@ -0,0 +1,37 @@
+// var name="a";
+// switch(name){
+//     case "a":
+//     console.log("you are a");
+//     break;
+//     case "b":
+//     console.log("you are b");
+//     break;
+//     case "c":
+//     console.log("you are c");
+//     break;
+//     default:
+//         console.log("name is different");
+// }
+
+var name=20;
+// switch(userage==17)
+// var flagforswitch=true
+switch(true){
+    case (name >18):
+        console.log("allow for DL");
+        // flagforswitch=false;
+        break;
+    case (name<18):
+    console.log("not allow");
+    // flagforswitch=false;
+
+     break;
+     case(name>80):
+     console.log("over age");
+    //  flagforswitch=false;
+
+     break;
+     default:
+        console.log("different");
+        break;
+}
