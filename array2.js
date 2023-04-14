0d3d5b342c3e8c95f50188b61f70d751bbda13ae
annusingh58 <127593919+annusingh58@users.noreply.github.com>
2023-04-14 11:06:07 -0400

    Create array2.js

---
+++ array2.js
@@ -0,0 +1,8 @@
+var fruits = ["mango","banana","graphes", "watermelon","orange"];
+console.log(fruits[0])
+console.log(fruits[1])
+console.log(fruits[2])
+console.log(fruits.length)
+for (var i = 0; i< fruits.length;i++){
+    console.length(fruits[i])
+}
