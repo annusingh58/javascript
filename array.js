d0cea19ca6d44b8f0cbac445d2caeb8a53fc7ef9
annusingh58 <127593919+annusingh58@users.noreply.github.com>
2023-04-14 09:57:43 -0400

    updates

---
+++ array.js
@@ -0,0 +1,96 @@
+// var array=[1,2,3,4,5,6,7,8,9];
+// array.pop();  //pop delete  the last of element .
+// array.pop();
+// array.pop();
+// array.pop();
+// array.pop();
+// consolse.log(array ,"array")
+
+
+// var array=[1,2,3,4,5,6,7,8,9];
+// array.pop();  //pop delete  the last of element .
+// array.pop();
+// array.pop();
+// array.shift();
+// array.shift();
+// consolse.log(array ,"array")
+
+// var array = [1,2,3,4,5,6,7,8,9];
+// array.pop();  //pop delete  the last of element .
+// array.pop();
+// array.pop();
+// array.pop();
+// array.pop();
+// consolse.log(array ,"array")
+// console.log(array.length,"length of array")
+
+// var array = [1,2,3,4,5,6,7,8,9,87,88,89,55];
+// array.unshift("anu","singh")
+// array.unshift("maan")
+// console.log(array,"array")
+
+// var array = [1,2,3,4,5,6,7,8,9,10,11,87,88,89,55];
+// array.push("anu")   // add element at last
+// array.pop();   //remove element from last
+
+
+// array.shift(); //remove element from start
+// array.unshift("anu","singh")  // add element at start 
+// array.unshift("maan")
+// console.log(array,"array");
+// console.log(array.length,"- length of array");
+// console.log(array.length,"array");
+
+// using foor loop
+
+var studentlist =["anu","kunal","ponam","abhi","shnel","swaraj"];
+console.log(studentlist[2])
+
+console.log(studentlist.length)
+for(var i=0; i < studentlist.length; i++){
+    console.log(studentlist[i],i)
+}
+
+for(var i=0; i < studentlist.length; i++){
+
+    if(studentlist[i]="swaraj"){
+        console.log("kunal is in oue data")
+
+    }
+}
+
+
+// var flagforswaraj =false;
+// for(var i=0; i < studentlist.length; i++){
+
+//     if(studentlist[i]="swaraj"){
+//         flagforswaraj ="true"
+//         console.log(" swaraj is in oue data")
+
+//     }
+
+// }
+// if(flagforswaraj == false){
+//     console.log(" swaraj is not in oue data")
+
+
+// }
+
+
+
+
+
+// for(var i=0; i < studentlist.length; i++){
+
+//     if(studentlist[i]=="swaraj" || studentlist[i]=="kunal"){
+//         console.log(" swaraj  or kunal is in oue data")
+
+//     }
+// }
+
+
+
+
+
+
+
